1d19bf1a52d76776f56f77a59d4a9ec1e214a5c9
Kunal Baghel <Kunalbaghel995@gmail.com>
2024-07-06 00:40:17 +0530

    get server changed

--- src/app/api/blog/get-all-blogs/route.js
+++ src/app/api/blog/get-all-blogs/route.js
@@ -2,32 +2,47 @@ import connectToDB from "@/database"
 import Blog from "@/models/blog"
 import { NextResponse } from "next/server"
 
-
 export async function GET(){
-    try{
-        await connectToDB()
+    try {
+        await connectToDB();
 
-        const blogData = await Blog.find({})
+        const blogData = await Blog.find({});
 
-        if(blogData && blogData.length){
+        if (blogData && blogData.length) {
             return NextResponse.json({
                 success: true,
                 data: blogData
-            })
-        }else{
+            }, {
+                headers: {
+                    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                    'Pragma': 'no-cache',
+                    'Expires': '0',
+                }
+            });
+        } else {
             return NextResponse.json({
                 success: false,
                 message: "Failed to get blogs"
-            })
+            }, {
+                headers: {
+                    'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                    'Pragma': 'no-cache',
+                    'Expires': '0',
+                }
+            });
         }
-
-
-    }catch(e){
-        console.log(e)
+    } catch (e) {
+        console.log(e);
 
         return NextResponse.json({
             success: false,
             message: "Something went wrong"
-        })
+        }, {
+            headers: {
+                'Cache-Control': 'no-store, no-cache, must-revalidate, proxy-revalidate',
+                'Pragma': 'no-cache',
+                'Expires': '0',
+            }
+        });
     }
 }
